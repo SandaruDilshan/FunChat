1cc55798a62137654b88c20cd7ba7123d82cce34
SandaruDilshan <jsandarudilshan@gmail.com>
2024-09-10 14:20:28 +0530

    install nodemon

--- back_end/server.js
+++ back_end/server.js
@@ -10,4 +10,8 @@ app.listen(5000, () =>
 //Test root route http://localhost:5000
 app.get("/", (req, res) =>{
     res.send("Hello World !");
-}); 
+});
+
+// instead "server": "node back_end/server.js" we use nodemon
+ // using node we have to stop and start server againg and again
+ //nodemon automatically update changes
