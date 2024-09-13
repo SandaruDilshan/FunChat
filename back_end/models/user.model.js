55a9feacdfa745c37c7e5a8856bade574ba94825
SandaruDilshan <jsandarudilshan@gmail.com>
2024-09-13 10:46:04 +0530

    Create user model and contoler

---
+++ back_end/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema =  new mongoose.Schema(
+    {
+        fullName:{
+            type:String,
+            required: true
+        },
+        userName:{
+            type:String,
+            required: true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+            minlength:6
+        },
+        gender:{
+            type:String,
+            required:true,
+            enum:["male","female"]
+        },
+        profilePic:{
+            type:String,
+            default:"",
+        },
+    }
+);
+
+const User = mongoose.model("User", userSchema); //Here we use singuler and first letter capital word for model name(like User)
+
+export default User;
