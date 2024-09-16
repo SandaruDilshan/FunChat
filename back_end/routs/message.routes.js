657d1f748b609557e871fdc0b4eebe8dc40edc74
SandaruDilshan <jsandarudilshan@gmail.com>
2024-09-16 23:48:05 +0530

    create get message function

--- back_end/routs/message.routes.js
+++ back_end/routs/message.routes.js
@@ -5,5 +5,7 @@ import protectRout from "../midleware/protectRout.js";
 const router = express.Router();
 
 router.post("/send/:id", protectRout, sendMessage); // First check protect rout function isuser log 
+router.get("/:id", protectRout, getdMessage); // First check protect rout function isuser log 
+
 
 export default router;
