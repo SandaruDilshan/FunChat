3620ff33e08e0607b52192e51f63e2ae7ab6ae61
Sandaru D Chandrasiri <130263550+SandaruDilshan@users.noreply.github.com>
2024-09-21 13:41:43 +0530

    Create search functionality and user message time and sepeerate render message tow user

--- front_end/src/components/messages/Message.jsx
+++ front_end/src/components/messages/Message.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand_store/useConversation';
+import { extractTime } from '../../utils/extractTime';
 
-function Message() {
+function Message({message}) {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const from_Me = message.senderId === authUser._id;  
+  const chatClassName = from_Me ? 'chat-end' : 'chat-start';
+  const profilePicture = from_Me ? authUser.profilePic : selectedConversation?.profilePic;
+  const chatColor = from_Me ? 'bg-blue-500' : "";
+  const formattedTime = extractTime(message.createdAt);
+  
   return (
-    <div className=' chat chat-end'>
+    <div className={`chat ${chatClassName}`} >
       <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img alt="Tailwind CSS chat bubble component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                <img alt="Tailwind CSS chat bubble component" src={profilePicture} />
             </div>       
       </div>
-      <div className="chat-bubble text-white ">You were the Chosen One!</div>
+      <div className={`chat-bubble text-white ${chatColor}`}>{message.message}</div>
       <div className='flex gap-2'>
-          <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-white">1.32</div>
-          <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-white">Delivered</div>
+          <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-white">{formattedTime}</div>
+          {/* <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-white">Delivered</div> */}
       </div>
       
     </div>
--- front_end/src/components/sidebar/SearchInput.jsx
+++ front_end/src/components/sidebar/SearchInput.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosSearch } from "react-icons/io";
+import useConversation from '../../zustand_store/useConversation';
+import useGetConversation from '../../hooks/useGetConversation';
+import toast from 'react-hot-toast';
 
 const SearchInput = () => {
+  const [Search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversation();
+
+  //Search functionality
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!Search) return;
+    if (Search.length < 3) {
+      return toast.error('Search term must be at least 3 characters long')
+    }
+
+    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(Search.toLowerCase()));
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    }
+    else {
+      toast.error("No such user found .!");
+    }
+  };
+
   return (
-    <form action="" className='flex item-center gap-2'>
-        <input type="text" placeholder='Search' className='input input-border border-full' />
+    <form onSubmit={handleSubmit} className='flex item-center gap-2'>
+      <input type="text" placeholder='Search' className='input input-border border-full'
+        value={Search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
         <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
             <IoIosSearch className='w-7 h-7 outline-none'/>
         </button>  
